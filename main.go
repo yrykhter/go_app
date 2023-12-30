1879ded15eadc6390213dd417bdbadc03f2fde7b
Yauheni Rykhter <ryhterrrr@mail.ru>
2023-12-30 19:25:48 +0400

    update main.go

--- main.go
+++ main.go
@@ -6,10 +6,15 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func main() {
-
+func setupRouter() *gin.Engine {
 	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(200, "pong")
+	})
+	return r
+}
 
+func APICalls(r *gin.Engine) {
 	r.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "Can use that paths: /info")
 	})
@@ -25,6 +30,10 @@ func main() {
 	api.GET("/info", func(c *gin.Context) {
 		c.String(http.StatusOK, "Server OK")
 	})
-	r.Run("localhost:8080")
+}
 
+func main() {
+	router := setupRouter()
+	APICalls(router)
+	router.Run(":8080")
 }
