2f181a9ad304e17d807be609dd8c87f17afdae16
Yauheni Rykhter <ryhterrrr@mail.ru>
2024-01-13 19:24:30 +0400

    add terraform code for the infrastructure

---
+++ infrastructure/main.tf
@@ -0,0 +1,6 @@
+module "infrastructure" {
+  cluster_name          = var.cluster_name
+  source                = "./modules/infrastructure"
+  google_project_region = var.google_project_region
+  google_project = var.google_project
+}
