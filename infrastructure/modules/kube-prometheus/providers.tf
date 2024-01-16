f6f8fd6faca9fe3edf0872d3681c5089b165d589
Yauheni Rykhter <ryhterrrr@mail.ru>
2024-01-16 13:41:12 +0400

    add module kube-prometheus for infrastructure

---
+++ infrastructure/modules/kube-prometheus/providers.tf
@@ -0,0 +1,12 @@
+# For authentication, the Helm provider can get its configuration by supplying a path to your kubeconfig file.
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+# Used to interact with the resources supported by Kubernetes.
+# The provider needs to be configured with the proper credentials before it can be used.
+provider "kubernetes" {
+  config_path = pathexpand(var.kube_config)
+}
